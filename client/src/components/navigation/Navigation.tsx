cf2f1167aebb1ee203b0ec1ba0d28e90882b8b9f
RadoslavK <RadoslavK@kentico.com>
2019-06-07 23:48:18 +0200

    accessible controller button

--- client/src/components/navigation/Navigation.tsx
+++ client/src/components/navigation/Navigation.tsx
@@ -1,9 +1,13 @@
+import { IsBotRunning, StartBot, StopBot } from "*/graphql_operations/controller.graphql";
+import { Button } from '@material-ui/core';
 import AppBar from '@material-ui/core/AppBar';
 import makeStyles from '@material-ui/core/styles/makeStyles';
 import Tab from '@material-ui/core/Tab';
 import Tabs from '@material-ui/core/Tabs';
 import React from 'react';
+import { useMutation, useQuery } from 'react-apollo-hooks';
 import { Link } from 'react-router-dom';
+import { IIsBotRunningQuery, IStartBotMutation, IStopBotMutation } from '../../_types/graphql';
 import { INavigationItem } from '../../_types/INavigationItem';
 import useReactRouter from 'use-react-router';
 
@@ -22,8 +26,25 @@ interface IProps {
 export const Navigation: React.FunctionComponent<IProps> = (props) => {
   const classes = useStyles(props.drawerWidth)({});
   const { location } = useReactRouter();
+  const { data, loading } = useQuery<IIsBotRunningQuery>(IsBotRunning);
+  const startBot = useMutation<IStartBotMutation>(StartBot, {
+    refetchQueries: [{ query: IsBotRunning }],
+  });
+  const stopBot = useMutation<IStopBotMutation>(StopBot, {
+    refetchQueries: [{ query: IsBotRunning }],
+  });
+
+  if (loading) {
+    return null;
+  }
 
   const currentItemIndex = location.pathname.startsWith('/villages') ? 1 : 0;
+
+  const {
+    isBotRunning,
+  } = data;
+
+  const onClick = async () => await isBotRunning ? stopBot() : startBot();
 
   return (
     <AppBar position="fixed" className={classes.appBar}>
@@ -40,6 +61,13 @@ export const Navigation: React.FunctionComponent<IProps> = (props) => {
             to={route.path}
           />
         ))}
+        <Button
+          variant="contained"
+          color="primary"
+          onClick={onClick}
+        >
+          {isBotRunning ? 'Stop bot' : 'Start bot'}
+        </Button>
       </Tabs>
     </AppBar>
   );
--- client/src/constants/paths.ts
+++ client/src/constants/paths.ts
@@ -1,6 +1,5 @@
 import { INavigationItem } from '../_types/INavigationItem';
 
 export const navigationItems: readonly INavigationItem[] = [
-  { path: '/', text: 'Home' },
   { path: '/villages', text: 'Villages' },
 ];
