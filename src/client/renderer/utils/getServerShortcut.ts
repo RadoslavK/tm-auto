fb4a6f1fb0eb8630cd0437c37398812140fa71d8
RadoslavK <RadoslavK@kentico.com>
2020-06-07 11:54:46 +0200

    Fix parsing server shortcut

--- src/client/renderer/utils/getServerShortcut.ts
+++ src/client/renderer/utils/getServerShortcut.ts
@@ -1,17 +1,22 @@
 export const getServerShortcut = (server: string): string => {
-  const speedMatch = /(x).*\.travian.*\.(.*)\/?/.exec(server);
+  const match = /https?:\/\/(.*?)\.(.*?)\.travian/.exec(server);
 
-  if (speedMatch && speedMatch.length === 3 && speedMatch[1] && speedMatch[2]) {
-    return `${speedMatch[2]}${speedMatch[1]}`;
+  if (!match) {
+    throw new Error('Failed to parse server shortcut');
   }
 
-  const match = /s(\d+).*\.travian.*\.(.*)\/?/.exec(server);
+  const type = match[1];
+  const country = match[2];
 
-  if (match && match.length === 3 && match[1] && match[2]) {
-    return `${match[2]}${match[1]}`;
+  const typeMatch = /tx(\d+)|ts(\d+)/.exec(type);
+
+  if (!typeMatch) {
+    throw new Error('Failed to parse server type');
   }
 
-  console.error(`Could not parse server shortcut, for server: ${server}`);
+  const typeName = typeMatch[1]
+    ? `${typeMatch[1]}x`
+    : typeMatch[2];
 
-  return server;
+  return `${country.toUpperCase()} ${typeName}`;
 };
