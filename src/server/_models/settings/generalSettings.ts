463a39726731c7f5478177524b5bff9ae67ca929
RadoslavK <RadoslavK@kentico.com>
2020-06-07 12:36:27 +0200

    Fix loading and merging general settings

--- src/server/_models/settings/generalSettings.ts
+++ src/server/_models/settings/generalSettings.ts
@@ -1,7 +1,14 @@
 import path from 'path';
 
+import { mergeDefaults } from '../../../_shared/merge';
+import { PartialFields } from '../../../_shared/types/fields.type';
+
 export class GeneralSettings {
   public readonly dataPath: string = path.join(__dirname, '.data');
   public readonly chromePath: string = 'C:/Program Files (x86)/Google/Chrome/Application/chrome.exe';
   public readonly headlessChrome: boolean = true;
+
+  constructor(params: PartialFields<GeneralSettings> = {}) {
+    mergeDefaults(this, params);
+  }
 }
