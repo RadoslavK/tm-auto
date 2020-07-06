5c0337f41cf5aaa46dba71fa60636764dfd286ca
RadoslavK <RadoslavK@kentico.com>
2020-07-06 14:18:06 +0200

    Fix read gold advantages

--- src/server/controller/actions/mentor/completeTasks/readGoldAdvantanges.ts
+++ src/server/controller/actions/mentor/completeTasks/readGoldAdvantanges.ts
@@ -3,13 +3,19 @@ import { getPage } from '../../../../browser/getPage';
 export const readGoldAdvantanges = async (): Promise<void> => {
   const page = await getPage();
 
-  await Promise.all([
+  const [, advantagesTab] = await Promise.all([
     page.click('#header .shop'),
-    page.waitForSelector('#paymentWizard'),
+    page.waitForSelector('#paymentWizard .tabButton.pros .text'),
   ]);
 
+  // so we can click the middle of the text
+  const rect = await page.evaluate((el) => {
+    const { top, left, width, height } = el.getBoundingClientRect();
+    return { top, left, width, height };
+  }, advantagesTab);
+
   await Promise.all([
-    page.click('#paymentWizard .tabButton.pros .text'),
+    page.mouse.click(rect.left + rect.width / 2, rect.top + rect.height / 2),
     page.waitForSelector('[name="featureName"]'),
   ]);
 
