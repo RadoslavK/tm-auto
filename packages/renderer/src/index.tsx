90d8ab9462c278bec4f7c67c1092369edf6e012c
RadoslavK <RadoslavK@kentico.com>
2021-03-30 19:05:24 +0200

    Add support for concurrent mode

--- packages/renderer/src/index.tsx
+++ packages/renderer/src/index.tsx
@@ -3,14 +3,30 @@ import ReactDOM from 'react-dom';
 
 import { App } from './applications/App.js';
 
-ReactDOM.render(
-  (
-    <React.StrictMode>
-      <App />
-    </React.StrictMode>
-  ),
-  document.getElementById('app'),
+const appElement: JSX.Element = (
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
+
+const rootNode = document.getElementById('app');
+
+if (!rootNode) {
+  throw new Error('Did not find app root node');
+}
+
+enum Mode {
+  Blocking,
+  Concurrent,
+}
+
+const mode = Mode.Blocking;
+
+if (mode === Mode.Blocking) {
+  ReactDOM.render(appElement, rootNode);
+} else {
+  ReactDOM.unstable_createRoot(rootNode).render(appElement);
+}
 
 if (import.meta.hot) {
   import.meta.hot.accept();
